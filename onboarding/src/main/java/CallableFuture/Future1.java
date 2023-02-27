86f787e8a8fbc262573fb349335a34f16ef763f7
rlxo4307 <rlxo4307.diquest.com>
2023-02-27 09:15:40 +0900

    2/24 commit

---
+++ onboarding/src/main/java/CallableFuture/Future1.java
@@ -0,0 +1,44 @@
+package CallableFuture;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class Future1 implements Future<Integer> {
+    int a;
+    int b;
+    Callable1 c1;
+    public Future1(){}
+    public Future1(Callable1 c1){
+        this.c1 = c1;
+    }
+    @Override
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        return false;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return false;
+    }
+
+    @Override
+    public boolean isDone() {
+        return false;
+    }
+
+    @Override
+    public Integer get() throws InterruptedException, ExecutionException {
+        try {
+            return c1.call();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Integer get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return null;
+    }
+}
--- onboarding/src/main/java/CallableFuture/Main.java
+++ onboarding/src/main/java/CallableFuture/Main.java
@@ -4,22 +4,21 @@ import java.util.Scanner;
 import java.util.concurrent.*;
 class Main {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        System.out.print("두 수를 입력하세요:");
-        Scanner sc = new Scanner(System.in);
-        Integer a = sc.nextInt();
-        Integer b = sc.nextInt();
-        System.out.println();
-
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-
-        Callable1 c1 = new Callable1(a, b);
-        Future<Integer> future = executor.submit(c1);
 
         int i = 0;
         while (i < 1000) {
+            System.out.print("두 수를 입력하세요:");
+            Scanner sc = new Scanner(System.in);
+            Integer a = sc.nextInt();
+            Integer b = sc.nextInt();
+            System.out.println();
+
+            Callable1 c1 = new Callable1(a, b);
+            Future<Integer> future = new Future1(c1);
             i += future.get();
+            System.out.println("Only maked by Future : " + i);
             System.out.println("Future Add Result [("+a+"+"+b+")배로 증가]:" + i);
         }
-        executor.shutdown();
+
     }
 }
