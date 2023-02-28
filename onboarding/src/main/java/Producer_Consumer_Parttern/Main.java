dcddd5fa17e2d51644d86f959392420c61e8fb85
rlxo4307 <rlxo4307.diquest.com>
2023-02-28 11:16:58 +0900

    2/28 commit

--- onboarding/src/main/java/Producer_Consumer_Parttern/Main.java
+++ onboarding/src/main/java/Producer_Consumer_Parttern/Main.java
@@ -1,7 +1,7 @@
 package Producer_Consumer_Parttern;
 class Main {
     public static void main(String[] args){
-        new Thread(new Consumer()).start();
         new Thread(new Producer()).start();
+        new Thread(new Consumer()).start();
     }
 }
