800aadf2bd734b20fc62e43dc4b240af0727dfa4
rlxo4307 <rlxo4307.diquest.com>
2023-03-03 13:43:11 +0900

    3/03 commit

--- onboarding/src/main/java/Producer_Consumer_Parttern/ConsumerTask.java
+++ onboarding/src/main/java/Producer_Consumer_Parttern/ConsumerTask.java
@@ -23,17 +23,16 @@ class ConsumerTask implements Runnable{
         if(z == 1) result = this.x + this.y;
         if(z == 2) result = this.x - this.y;
         if(z == 3) result = this.x * this.y;
-        if(z == 4 && this.y == 0) state = "zero divide";
         if(z == 4 && this.y != 0) result = (Integer) (this.x / this.y);
-        if(z < 1 && z > 4) state = "fail";
-
-        if(state.equals("success")){
-            System.out.println("소비자가 작업을 처리합니다 > 결과:" + Thread.currentThread().getName() + " : " + result);
-        }else if(state.equals("fail")){
-            System.out.println("소비자가 작업을 처리합니다 > 연산을 잘못 선택했습니다 | " + Thread.currentThread().getName());
-        }else if(state.equals("zero divide")){
+        if(z == 4 && this.y == 0) {
+            state = "zero divide";
             System.out.println("소비자가 작업을 처리합니다 > 나눗셈 분모가 0입니다 | " + Thread.currentThread().getName());
         }
+        if(z < 1 && z > 4) {
+            state = "fail";
+            System.out.println("소비자가 작업을 처리합니다 > 연산을 잘못 선택했습니다 | " + Thread.currentThread().getName());
+        }
+        if(state.equals("success")) System.out.println("소비자가 작업을 처리합니다 > 결과:" + Thread.currentThread().getName() + " : " + result);
     }
 }
 
