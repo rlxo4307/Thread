6c1b843425dc5552c6101b2c2e9a53ed86c1c940
rlxo4307 <rlxo4307.diquest.com>
2023-03-06 17:00:24 +0900

    3/06 commit

--- onboarding/src/main/java/객체지향생활체조원칙/한줄에점하나만찍는다.java
+++ onboarding/src/main/java/객체지향생활체조원칙/한줄에점하나만찍는다.java
@@ -1,23 +1,23 @@
 package 객체지향생활체조원칙;
 
 class 한줄에점하나만찍는다 {
-    Korea ko = new Korea(new Name());
+    Korea1 ko = new Korea1(new Name1());
     void KoreaNameIsWhat(){
         ko.koreaName().get();
     }
 }
 
-class Korea {
-    private final Name korea;
-    Korea(Name korea) {
+class Korea1 {
+    private final Name1 korea;
+    Korea1(Name1 korea) {
         this.korea = korea;
     }
-    Name koreaName(){
+    Name1 koreaName(){
         return korea;
     }
 }
 
-class Name {
+class Name1 {
     private String name = "한국";
     String get(){
         return name;
--- onboarding/src/main/java/객체지향생활체조원칙/한줄에점하나만찍는다_sol.java
+++ onboarding/src/main/java/객체지향생활체조원칙/한줄에점하나만찍는다_sol.java
@@ -1,27 +1,25 @@
 package 객체지향생활체조원칙;
 
-public class 한줄에점하나만찍는다_sol {
-    class 한줄에점하나만찍는다 {
-        Korea ko = new Korea(new Name());
+ class 한줄에점하나만찍는다_sol {
+        Korea1 ko = new Korea1(new Name1());
         void KoreaNameIsWhat(){
             ko.koreaName();
         }
     }
 
-    class Korea {
-        private final Name korea;
-        Korea(Name korea) {
-            this.korea = korea;
-        }
-        String koreaName(){
-            return korea.get();
-        }
+class Korea2 {
+    private final Name1 korea;
+    Korea2(Name1 korea) {
+        this.korea = korea;
     }
+    String koreaName(){
+        return korea.get();
+    }
+}
 
-    class Name {
-        private String name = "한국";
-        String get(){
-            return name;
-        }
+class Name2 {
+    private String name = "한국";
+    String get(){
+        return name;
     }
 }
