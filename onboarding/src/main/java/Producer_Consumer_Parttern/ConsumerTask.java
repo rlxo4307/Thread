f539c20c8362b76f1420c54284c0c36b79a05891
rlxo4307 <rlxo4307.diquest.com>
2023-03-06 09:36:23 +0900

    3/03 commit

--- onboarding/src/main/java/Producer_Consumer_Parttern/ConsumerTask.java
+++ onboarding/src/main/java/Producer_Consumer_Parttern/ConsumerTask.java
@@ -2,12 +2,12 @@ package Producer_Consumer_Parttern;
 class ConsumerTask implements Runnable{
     private final int x;
     private final int y;
-    private final int z;
+    private final int sign;
     private String state;
-    public ConsumerTask(int x, int y, int z, String state) {
+    public ConsumerTask(int x, int y, int sign, String state) {
         this.x = x;
         this.y = y;
-        this.z = z;
+        this.sign = sign;
         this.state = state;
     }
     @Override
@@ -20,15 +20,15 @@ class ConsumerTask implements Runnable{
 
         int result = 0;
 
-        if(z == 1) result = this.x + this.y;
-        if(z == 2) result = this.x - this.y;
-        if(z == 3) result = this.x * this.y;
-        if(z == 4 && this.y != 0) result = (Integer) (this.x / this.y);
-        if(z == 4 && this.y == 0) {
+        if(sign == 1) result = this.x + this.y;
+        if(sign == 2) result = this.x - this.y;
+        if(sign == 3) result = this.x * this.y;
+        if(sign == 4 && this.y != 0) result = (Integer) (this.x / this.y);
+        if(sign == 4 && this.y == 0) {
             state = "zero divide";
             System.out.println("소비자가 작업을 처리합니다 > 나눗셈 분모가 0입니다 | " + Thread.currentThread().getName());
         }
-        if(z < 1 && z > 4) {
+        if(sign < 1 && sign > 4) {
             state = "fail";
             System.out.println("소비자가 작업을 처리합니다 > 연산을 잘못 선택했습니다 | " + Thread.currentThread().getName());
         }
--- onboarding/src/main/java/Producer_Consumer_Parttern/ProducerTask.java
+++ onboarding/src/main/java/Producer_Consumer_Parttern/ProducerTask.java
@@ -1,11 +1,11 @@
 package Producer_Consumer_Parttern;
 class ProducerTask implements Runnable{
-    private int x, y, z;
+    private int x, y, sign;
     private String state;
-    ProducerTask(int x, int y, int z, String state){
+    ProducerTask(int x, int y, int sign, String state){
         this.x = x;
         this.y = y;
-        this.z = z;
+        this.sign = sign;
         this.state = state;
     }
     @Override
@@ -13,7 +13,7 @@ class ProducerTask implements Runnable{
         try {
             Thread.sleep(1000L);
             System.out.println("생성자가 작업(사칙연산)을 생성합니다 | "+Thread.currentThread().getName());
-            SharedResource.getInstance().put(new ConsumerTask(this.x, this.y, this.z, this.state));
+            SharedResource.getInstance().put(new ConsumerTask(this.x, this.y, this.sign, this.state));
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
