7618a56df80942367bba3854c195df2e3456c614
rlxo4307 <rlxo4307.diquest.com>
2023-03-06 17:07:51 +0900

    3/06 commit

--- onboarding/src/main/java/CodeSmellList/다운캐스팅.java
+++ onboarding/src/main/java/CodeSmellList/다운캐스팅.java
@@ -1,6 +1,7 @@
 package CodeSmellList;
 
 class 다운캐스팅 extends 커다란클래스{
-    다운캐스팅 downBmi = (다운캐스팅)new 커다란클래스();
+    커다란클래스 hugeClass = new 커다란클래스();
+    다운캐스팅 downBmi = (다운캐스팅)hugeClass;
     double bmi = downBmi.bmi(178, 66);
 }
