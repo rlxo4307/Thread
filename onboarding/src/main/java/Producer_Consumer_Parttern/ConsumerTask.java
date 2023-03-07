d2659432429f8c6b054c25dbd417f9f00987cdc3
rlxo4307 <rlxo4307.diquest.com>
2023-03-07 16:35:47 +0900

    3/07 commit

--- onboarding/src/main/java/Producer_Consumer_Parttern/ConsumerTask.java
+++ onboarding/src/main/java/Producer_Consumer_Parttern/ConsumerTask.java
@@ -17,21 +17,23 @@ class ConsumerTask implements Runnable{
         }
 
         int result = 0;
+        String ment = "소비자가 작업을 처리합니다 | ";
+        String thread = Thread.currentThread().getName();
 
         if(sign == 1) result = this.x + this.y;
         if(sign == 2) result = this.x - this.y;
         if(sign == 3) result = this.x * this.y;
         if(sign == 4 && this.y != 0) result = (Integer) (this.x / this.y);
         if(sign == 4 && this.y == 0) {
-            System.out.println("소비자가 작업을 처리합니다 | 나눗셈 분모가 0입니다 | " + Thread.currentThread().getName());
+            System.out.println(ment + "나눗셈 분모가 0입니다 | " + thread);
             return;
         }
         if(sign == 1 || sign == 2 || sign == 3 || sign == 4) {
-            System.out.println("소비자가 작업을 처리합니다 | 결과:" + result +" | " + Thread.currentThread().getName());
+            System.out.println(ment + "결과:" + result +" | " + thread);
             return;
         }
         if(sign != 1 && sign != 2 && sign != 3 && sign != 4) {
-            System.out.println("소비자가 작업을 처리합니다 | 연산을 잘못 선택했습니다 | " + Thread.currentThread().getName());
+            System.out.println(ment + "연산을 잘못 선택했습니다 | " + thread);
         }
     }
 }
