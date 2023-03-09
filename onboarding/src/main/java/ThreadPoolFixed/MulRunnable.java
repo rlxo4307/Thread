56ca2bbb503c81efba2bc5b918140ff18785cbbe
rlxo4307 <rlxo4307.diquest.com>
2023-03-09 14:49:12 +0900

    3/09 commit

--- onboarding/src/main/java/ThreadPoolFixed/MulRunnable.java
+++ onboarding/src/main/java/ThreadPoolFixed/MulRunnable.java
@@ -8,14 +8,10 @@ class MulRunnable implements Runnable{
         System.out.println(Thread.currentThread().getName()+"가 시작되었습니다");
         si.add();
         System.out.println(Thread.currentThread().getName()+"가 종료되었습니다");
-        sleepThread();
-    }
-
-    private void sleepThread(){
         try {
             Thread.sleep(1000L);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
